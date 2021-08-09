81d27960742155ea65627dc318e2630fd19e9747
Joe Ostrander <2170653+joeostrander@users.noreply.github.com>
2021-08-09 11:31:05 -0500

    icon, tray mode, run at startup

--- COM_MON/Form1.Designer.cs
+++ COM_MON/Form1.Designer.cs
@@ -29,9 +29,16 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.runAtStartupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStripTray.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -54,17 +61,64 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStripTray;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStripTray
+            // 
+            this.contextMenuStripTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runAtStartupToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStripTray.Name = "contextMenuStripTray";
+            this.contextMenuStripTray.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStripTray.Size = new System.Drawing.Size(150, 70);
+            // 
+            // runAtStartupToolStripMenuItem
+            // 
+            this.runAtStartupToolStripMenuItem.CheckOnClick = true;
+            this.runAtStartupToolStripMenuItem.Name = "runAtStartupToolStripMenuItem";
+            this.runAtStartupToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.runAtStartupToolStripMenuItem.Text = "&Run at Startup";
+            this.runAtStartupToolStripMenuItem.Click += new System.EventHandler(this.runAtStartupToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(438, 291);
             this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Move += new System.EventHandler(this.Form1_Move);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStripTray.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -73,6 +127,11 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripTray;
+        private System.Windows.Forms.ToolStripMenuItem runAtStartupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- COM_MON/Form1.cs
+++ COM_MON/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,10 +11,19 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * TODO:  
+ * 
+ */
+
+
 namespace COM_MON
 {
     public partial class Form1 : Form
     {
+
+        private bool boolStartup = false;
+
         //private static List<KeyValuePair<string, string>> listPorts = new List<KeyValuePair<string, string>>();
 
         private struct myPort
@@ -33,6 +43,9 @@ namespace COM_MON
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = Application.ProductName;
+
+            notifyIcon1.Text = Application.ProductName;
+            LoadRegistrySettings();
 
             dataGridView1.DataSource = listPorts;
             dataGridView1.Columns[0].DefaultCellStyle.Format = "yyyy.MM.dd HH:mm:ss";
@@ -117,5 +130,156 @@ namespace COM_MON
         {
             Get_Com_Ports();
         }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.ShowDialog();
+        }
+
+        private void runAtStartupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            boolStartup = runAtStartupToolStripMenuItem.Checked;
+            SaveRegistrySettings();
+        }
+
+
+        private bool SaveRegistrySettings()
+        {
+            try
+            {
+                RegistryKey regKey_RUN;
+                regKey_RUN = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+                RegistryKey appRegKey;
+                appRegKey = Registry.CurrentUser.OpenSubKey("Software\\" + Application.ProductName, true);
+
+                //Startup 
+                appRegKey.SetValue("RunAtStartup", boolStartup, RegistryValueKind.DWord);
+
+                //if boolStartup..., add executable path to the Run registry key
+                if (boolStartup)
+                {
+                    regKey_RUN.SetValue(Application.ProductName, Application.ExecutablePath);
+                }
+                else
+                {
+                    if (regKey_RUN.GetValueNames().Contains(Application.ProductName))
+                    {
+                        regKey_RUN.DeleteValue(Application.ProductName);
+                    }
+
+                }
+
+                regKey_RUN.Close();
+                appRegKey.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exeption in SaveRegistrySettings()");
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void LoadRegistrySettings()
+        {
+            RegistryKey regKey_RUN;
+            regKey_RUN = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            RegistryKey appRegKey;
+            appRegKey = Registry.CurrentUser.OpenSubKey("Software\\" + Application.ProductName, true);
+
+            if (appRegKey == null)
+            {
+                // Create the key
+                Registry.CurrentUser.CreateSubKey("Software\\" + Application.ProductName);
+                appRegKey = Registry.CurrentUser.OpenSubKey("Software\\" + Application.ProductName, true);
+                if (appRegKey == null)
+                {
+                    regKey_RUN.Close();
+                    return;
+                }
+
+                //Ask user if they want it to launch auto...
+                if (MessageBox.Show("Launch " + Application.ProductName + " at Startup?", Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    boolStartup = true;
+                }
+                else
+                {
+                    boolStartup = false;
+                }
+            }
+            else
+            {
+                //Load current Startup Value and set it in the interface
+                boolStartup = (int)appRegKey.GetValue("RunAtStartup", 0) == 1 ? true : false;
+
+            }
+
+            regKey_RUN.Close();
+            appRegKey.Close();
+
+            SaveRegistrySettings();
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Text = Application.ProductName;
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                HideMe();
+            }
+        }
+
+        private void HideMe()
+        {
+            this.Hide();
+            notifyIcon1.ShowBalloonTip(2000, Application.ProductName, "Click to show", ToolTipIcon.Info);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ShowMe();
+            }
+        }
+
+        private void ShowMe()
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+            if (this.Location.X < 0 || this.Location.Y < 0) { this.CenterToScreen(); }
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                HideMe();
+            }
+            else
+            {
+                ShowMe();
+            }
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            ShowMe();
+        }
     }
+
+
 }
